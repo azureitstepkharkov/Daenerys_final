ab5bd9e0c8f1fccbb3e8c367b40acf046fcddaaa
SomeDevelop <v.bohdanovskyi@gmail.com>
2018-05-17 11:09:04 +0300

    Bohdanovskyi

---
+++ Daenerys/Assets/Scripts/CameraMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //ландшафтная ориентация 
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+	
+	// Update is called once per frame
+	
+}
